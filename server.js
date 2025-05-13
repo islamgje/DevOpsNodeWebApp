22a7b640376e1d88c9573329ccf2070fdda0df3e
islamgje <islamgje@students.zhaw.ch>
2025-05-14 00:54:52 +0200

    v2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course! :-) by islamgje. Test für Repo-09. Automatischer Cloud Deploy auf Render.com by islamgje');
+  res.send('(-: Hello FS2025 DevOps Course! :-) by islamgje. Test für Repo-09. Automatischer Cloud Deploy auf Render.com by islamgje v2');
 });
 
 app.listen(PORT, HOST);
