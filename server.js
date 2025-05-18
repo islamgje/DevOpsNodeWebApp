c4ed764bad8fd14693a7aea08e86317d13694719
islamgje <islamgje@students.zhaw.ch>
2025-05-18 22:50:52 +0200

    test screencast

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('(-: Hello FS2025 DevOps Course! :-) by islamgje. Test für Repo-09. Automatischer Cloud Deploy auf Render.com by islamgje v8');
+  res.send('(-: Hello FS2025 DevOps Course! :-) by islamgje. Test für Repo-09. Automatischer Cloud Deploy auf Render.com by islamgje v8 Test Screencast');
 });
 
 app.listen(PORT, HOST);
